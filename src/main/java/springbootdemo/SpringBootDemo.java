fef11b6d84d40cd05ca0756f16696f58193ed7f2
root <root@localhost.localdomain>
2018-07-01 07:03:58 +0200

    New update

---
+++ src/main/java/springbootdemo/SpringBootDemo.java
@@ -0,0 +1,27 @@
+package springbootdemo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SpringBootDemo {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		
+		return "Hallo World";
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		SpringApplication.run(SpringBootDemo.class, args);
+	}
+	
+	
+
+}
